87bb97db1f6e021ebec253d2f99f98e53f52b598
Adam Beckingham <beckadamtheartist@gmail.com>
2024-03-29 11:40:20 -0600

    Update web demos so that they hopefully work better on mobile

--- docs/demos/mandelbrot/sketch.js
+++ docs/demos/mandelbrot/sketch.js
@@ -5,17 +5,45 @@ let currentHeight = 1;
 let max_iterations = 12;
 let scale = 1;
 let offset = [-1, -1];
+let redraw = false;
 
 // function keyPressed() {
 	// return !(keyCode == UP_ARROW || keyCode == DOWN_ARROW || keyCode == LEFT_ARROW || keyCode == ARROW);
 // }
 
-function draw() {
-	let redraw = false;
+function resetViewport() {
+	document.getElementById("position_x").setAttribute("value", -100);
+	document.getElementById("position_y").setAttribute("value", 0);
+	document.getElementById("scale").setAttribute("value", 3);
+}
 
+function mouseDragged() {
+	const s = 1;
+	document.getElementById("position_x").setAttribute("value", offset[0] - s*movedX);
+	document.getElementById("position_y").setAttribute("value", offset[1] - s*movedY);
+	redraw = true;
+	return true;
+}
+
+function mouseWheel(event) {
+	const factor = event.delta>0 ? 1.25 : (1.0 / 1.25);
+	const s = float(document.getElementById("scale").value);
+	document.getElementById("scale").setAttribute("value", s * factor);
+	document.getElementById("position_x").setAttribute("value", (offset[0] / factor));
+	document.getElementById("position_y").setAttribute("value", (offset[1] / factor));
+	return false;
+}
+
+function draw() {
 	let w = float(document.getElementById("canvas_width").value);
 	let h = float(document.getElementById("canvas_height").value);
+	document.getElementById("canvas_width").value = min(1024, w);
+	document.getElementById("canvas_height").value = min(1024, h);
 	if (abs(w - currentWidth) >= epsilon || abs(h - currentHeight) >= epsilon) {
+		offset[0] = offset[0] * w / currentWidth;
+		offset[1] = offset[1] * h / currentHeight;
+		document.getElementById("position_x").setAttribute("value", offset[0]);
+		document.getElementById("position_y").setAttribute("value", offset[1]);
 		currentWidth = w;
 		currentHeight = h;
 		updateCanvas(w, h);
@@ -23,38 +51,6 @@ function draw() {
 		redraw = true;
 	}
 
-	const factor = 1.25;
-	const s = float(document.getElementById("scale").value);
-	if (keyIsDown(UP_ARROW)) {
-		document.getElementById("position_y").setAttribute("value", offset[1] - s*0.0625);
-		redraw = true;
-	}
-	if (keyIsDown(DOWN_ARROW)) {
-		document.getElementById("position_y").setAttribute("value", offset[1] + s*0.0625);
-		redraw = true;
-	}
-	if (keyIsDown(LEFT_ARROW)) {
-		document.getElementById("position_x").setAttribute("value", offset[0] - s*0.0625);
-		redraw = true;
-	}
-	if (keyIsDown(RIGHT_ARROW)) {
-		document.getElementById("position_x").setAttribute("value", offset[0] + s*0.0625);
-		redraw = true;
-	}
-	// '+' keycodes
-	if (keyIsDown(107) || keyIsDown(187) || keyIsDown(61)) {
-		document.getElementById("scale").setAttribute("value", s / factor);
-		document.getElementById("position_x").setAttribute("value", offset[0] + 0.125*s);
-		document.getElementById("position_y").setAttribute("value", offset[1] + 0.125*s);
-		redraw = true;
-	}
-	// '-' keycodes
-	if (keyIsDown(109) || keyIsDown(189) || keyIsDown(173)) {
-		document.getElementById("scale").setAttribute("value", s * factor);
-		document.getElementById("position_x").setAttribute("value", offset[0] - 0.125*s*factor);
-		document.getElementById("position_y").setAttribute("value", offset[1] - 0.125*s*factor);
-		redraw = true;
-	}
 	if (redraw || keyIsDown(ENTER)) {
 		// console.log("redrawing");
 		// console.log(document.getElementById("position_x").value, document.getElementById("position_y").value, document.getElementById("scale").value)
@@ -65,18 +61,26 @@ function draw() {
 function setup() {
 	pixelDensity(1);
 	createCanvas(1, 1);
-	updateCanvas(1024, 1024);
+	updateCanvas(512, 512);
 	drawfractal();
 }
 
 function updateCanvas(x, y) {
+	x = min(x, 1024);
+	y = min(y, 1024);
 	resizeCanvas(x, y);
-	document.getElementById("defaultCanvas0").style['height'] = "512px";
-	document.getElementById("defaultCanvas0").style['width'] = "512px";
+	updateCanvasElement(x, y);
+}
+function updateCanvasElement(w, h) {
+	w = min(w, top.innerWidth);
+	h = min(h, top.innerHeight);
+	document.getElementById("defaultCanvas0").style['height'] = `{w}px`;
+	document.getElementById("defaultCanvas0").style['width'] = `{h}px`;
 }
 
 function px(x, y, c) {
 	let i = (int(y)*currentWidth + int(x)) * 4;
+	c = min(255, max(0, int(c)));
 	pixels[i+0] = c;
 	pixels[i+1] = c;
 	pixels[i+2] = c;
@@ -85,20 +89,26 @@ function px(x, y, c) {
 
 function drawfractal() {
 	background(0);
+	let vmin = float(document.getElementById("minvalue").value)
+	let vmax = float(document.getElementById("maxvalue").value)
+	let vscale = float(document.getElementById("valuescale").value)
+	let oscale = float(document.getElementById("finalscale").value)
+	let exp = float(document.getElementById("exponent").value);
 	offset[0] = float(document.getElementById("position_x").value);
 	offset[1] = float(document.getElementById("position_y").value);
 	scale = float(document.getElementById("scale").value) / currentWidth;
 	max_iterations = float(document.getElementById("iterations").value);
+	document.getElementById("iterations").value = max_iterations = min(max_iterations, 100);
 	step = max(1, float(document.getElementById("step").value));
-	exp = float(document.getElementById("exponent").value);
 	// const step = 1;
 	loadPixels();
 	for (let y=0; y<currentHeight; y+=step) {
 		for (let x=0; x<currentWidth; x+=step) {
-			let c = int(mandelbrot(x*scale+offset[0], y*scale+offset[1]));
-			px(x+0, y+0, 55 + 200 * pow(c, exp) / pow(max_iterations, exp));
+			let c = mandelbrot((x+offset[0]-currentWidth*0.5)*scale, (y+offset[1]-currentHeight*0.5)*scale);
+			px(x, y, vmin + oscale * pow(vscale * c / max_iterations, exp) * (vmax - vmin));
 		}
 	}
+	// px(currentHeight/2, currentWidth/2, 0);
 	updatePixels();
 }
 
---
+++ localtest_testing.bat
@@ -0,0 +1,3 @@
+pushd testing
+python -m http.server 80
+popd
